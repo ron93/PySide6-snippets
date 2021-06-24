0dd9128b8529498e47af188e73156bf0ba1e8a7b
ron93 <ronmkare1@gmail.com>
2021-06-24 15:35:00 +0300

    events component -- template from file

---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector:'events-list',
+    templateUrl:'./events-list.component.html'
+})
+
+export class EventsListComponent {
+    events = {
+        id:1,
+        name:'Angular connect',
+        date:'9/07/2020',
+        time:'10:00 am',
+        price:600.00,
+        imageUrl:'/assets/images/angularconnect-shield.png',
+        location :{
+            address:'1057 Nairobi drive',
+            city:'Nairobi',
+            country:'Kenya'
+        }
+    }
+
+}
