38dba2b8f374d79de26e383d5c7711b6fd447034
ron93 <ronmkare1@gmail.com>
2021-06-24 10:00:18 +0300

    events project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template:`
+  <h2>Hello world</h2>
+  `
+  ,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
