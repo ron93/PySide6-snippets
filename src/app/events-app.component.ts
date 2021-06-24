fae3f921ee25d0daadd077394d91445e1a011cb4
ron93 <ronmkare1@gmail.com>
2021-06-24 13:00:21 +0300

    events-list-cmponent -- data binding

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  // templateUrl: './app.component.html',
   template:'<events-list></events-list>'
 })
 export class EventsAppComponent {
