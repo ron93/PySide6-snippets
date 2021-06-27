488594f53fdf557ad682c621285f19f61def2478
ron93 <ronmkare1@gmail.com>
2021-06-27 12:26:00 +0300

    removed events list from template to create events thumbnail and added input parameter for events thumbnail

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 
 export class EventsListComponent {
-    events = {
+    event = {
         id:1,
         name:'Angular connect',
         date:'9/07/2020',
