7b197777b58d225d0fac7abdfa53e4040b71e397
ron93 <ronmkare1@gmail.com>
2021-06-28 11:20:21 +0300

    added safe event navigator

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -7,13 +7,13 @@ import { Component,Input } from '@angular/core';
     `
         <div class="well hovewell thumbnail" >
         <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
+        <div>Date: {{event?.date}}</div>
+        <div>Time: {{event?.time}}</div>
+        <div>Price: \${{event?.price}}</div>
         <div>
-            <span>Location: {{event.location.address}}</span>
+            <span>Location: {{event?.location?.address}}</span>
             <span>&nbsp;</span>
-            <span>{{event.location.city}}, {{event.location.country}}</span>
+            <span>{{event?.location?.city}}, {{event?.location?.country}}</span>
 
         </div>
         </div>
