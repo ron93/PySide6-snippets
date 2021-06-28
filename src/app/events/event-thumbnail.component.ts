f889e2cbc1ca4e3fa990904203daa12bda6cd113
ron93 <ronmkare1@gmail.com>
2021-06-28 11:13:44 +0300

    thumbnail styling

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -17,8 +17,16 @@ import { Component,Input } from '@angular/core';
 
         </div>
         </div>
-    `
+    `,
+    styles:[
+        `
+        .thumbnail { min-height:210px; }
+        .pad-left {margin-left: 10px; }
+        .well div {color :#bbb; }
+        `
+    ]
 })
+
 
 export class EventThumbnailComponent {
  
