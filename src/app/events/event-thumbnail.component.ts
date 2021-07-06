393ef099ca76f07d2b36ed6e8b549525fd705eaf
ron93 <ronmkare1@gmail.com>
2021-07-06 11:28:08 +0300

    ngClass -- getBackground ->change background for thumbnail depending on event start time

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,10 +5,10 @@ import { Component,Input } from '@angular/core';
     selector:'event-thumbnail',
     template:
     `
-        <div class="well hovewell thumbnail" >
+        <div class="well hovewell thumbnail" [ngClass] ="getBackground()">
         <h2>{{event.name}}</h2>
         <div>Date: {{event?.date}}</div>
-        <div [ngClass]="{green: event?.time === '8:00 am',bold:event?.time === '8:00 am'}" [ngSwitch]="event?.time">
+        <div [ngClass]="{green : event?.time === '8:00 am', bold: event?.time === '8:00 am'}" [ngSwitch]="event?.time">
         Time: {{event?.time}}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
@@ -22,13 +22,14 @@ import { Component,Input } from '@angular/core';
             <span>{{event?.location?.city}}, {{event?.location?.country}}</span>
 
         </div>
-        <div *ngIf="event?.onlineUrl">
-        Online URL: {{event?.onlineUrl}}
-        </div>
+      
         </div>
     `,
     styles:[
         `
+        .bg { background-color:green !important;}
+        .bgten {backgroung-color : yellow !important;}
+
         .green {color: #003300 !important;}
         .bold { font-weight: bold;}
         .thumbnail { min-height:210px; }
@@ -43,4 +44,17 @@ export class EventThumbnailComponent {
  
     @Input() event:any 
 
-}
+    getBackground() {
+        if (this.event && this.event?.time === '8:00 am')
+            return ['bg', 'green']
+            
+        else if (this.event && this.event?.time === '10:00 am')
+            return['bgten','green']
+
+        return []
+
+
+        
+    }
+
+    }
