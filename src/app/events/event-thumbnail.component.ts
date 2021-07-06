773df4f5bfb50f571bd447ff5dd44ca7e12722e7
ron93 <ronmkare1@gmail.com>
2021-07-06 12:02:30 +0300

    getTimes func -> implemented ngStyle directive to change style per event start time

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,7 +8,7 @@ import { Component,Input } from '@angular/core';
         <div class="well hovewell thumbnail" [ngClass] ="getBackground()">
         <h2>{{event.name}}</h2>
         <div>Date: {{event?.date}}</div>
-        <div [ngClass]="{green : event?.time === '8:00 am', bold: event?.time === '8:00 am'}" [ngSwitch]="event?.time">
+        <div [ngStyle]="getTimes()" [ngSwitch]="event?.time">
         Time: {{event?.time}}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
@@ -28,8 +28,9 @@ import { Component,Input } from '@angular/core';
     styles:[
         `
         .bg { background-color:green !important;}
-        .bgten {backgroung-color : yellow !important;}
-
+        .bgten { background-color:white !important;}
+        .bg-default { background-color:grey !important;}
+         .blue {color:yellow;}   
         .green {color: #003300 !important;}
         .bold { font-weight: bold;}
         .thumbnail { min-height:210px; }
@@ -48,13 +49,23 @@ export class EventThumbnailComponent {
         if (this.event && this.event?.time === '8:00 am')
             return ['bg', 'green']
             
+           //bigten not working but green works(why??) -- solved it might be color name delegation
         else if (this.event && this.event?.time === '10:00 am')
             return['bgten','green']
+        else return ['bg-default']
 
         return []
-
-
         
+    }
+    getTimes() {
+        if (this.event && this.event?.time === '8:00 am')
+            return {color:'yellow','font-size':'20px'}
+            
+           //bigten not working but green works(why??) -- solved it might be color name delegation
+        else if (this.event && this.event?.time === '10:00 am')
+        return {color:'green','font-size':'20px'}
+
+        return {}
     }
 
     }
